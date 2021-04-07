ccf434dc376305b0278c799b8eafd63711066060
adrishyantee <adrishyantee@gmail.com>
2021-04-07 09:50:42 +0530

    added sol of april long

--- CP/Codechef/April Long Challenge/WorthyMatrix.cpp
+++ CP/Codechef/April Long Challenge/WorthyMatrix.cpp
@@ -1,53 +1,57 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define ll long long int
 
-#define ll long long int 
-const int N=9999;
 
-ll getAvg(ll r1, ll r2, ll c1, ll c2,
-           ll dp[N + 1][N + 1])
-{
-    return (dp[r2][c2] - dp[r2][c1] - dp[r1][c2]
-           + dp[r1][c1])/2;
-}
-
-bool AvgFound(ll K, ll S, ll grid[N+1])
-{
-    ll dp[N + 1][N + 1];
- 
-    for (ll i = 0; i < N; i++)
-        for (ll j = 0; j < N; j++)
-            dp[i + 1][j + 1] = dp[i + 1][j] + dp[i][j + 1]
-                               - dp[i][j] + grid[i][j];
-    for (ll i = 0; i < N; i++)
-        for (ll j = 0; j < N; j++) {
-            ll avg = getAvg(i, i + K, j, j + K, dp);
- 
-            if (avg >= S)
-                return true;
-        }
-    return false;
+ll min(ll a ,ll b){
+    return (a<b)?a:b;
 }
 
 int main(){
     int T;
     cin>>T;
-    while(T--){
-    ll n,m,K;
-    ll count=0,sum=0;
 
-    cin>>n>>m>>K;
-    ll arr[n][m];
+    while(T-- !=0){
+    ll n,m,k;
     
-     for (ll i = 0; i < n; i++)
-        for (ll j = 0; j < m; j++) 
+
+    cin>>n>>m>>k;
+    double arr[n+1][m+1];
+    
+    for (ll i = 0; i <= n; i++){
+        for (ll j = 0; j <= m; j++) {
+            if(i==0 || j==0){
+                arr[i][j=0];
+            }
+            else
             cin>>arr[i][j];
-
-    for(ll i=1;i<N;i++)
-       if(AvgFound(i,K,arr[])==true)
-            count++;
-    cout<<count<<endl;
-}
-
+            }
+        }
+    for(ll i=0;i<=n;i++){
+        double pre = 0;
+        for(ll j=0; j<=m;j++){
+            arr[i][j]+=pre;
+            pre = arr[i][j];
+        }
+    }
+    for(ll j=0;j<=m;j++){
+        double pre =0;
+        for(ll i=0; i<=n;i++){
+            arr[i][j]+=pre;
+            pre=arr[i][j];
+        }
+    }
+    ll mini = min(m,n);
+    ll ans=0;
+    for(ll len=1;len<=mini;len++){
+        for(ll i=len;i<=n;i++){
+            for(ll j=len;j<=m;j++){
+                if((arr[i][j]+arr[i-len][j-len]-arr[i][j-len]-arr[i-len][j])/(len*len) >=k)
+                ans++;
+            }
+        }
+    }
+    cout<<ans<<endl;
+    }
 return 0;
 }
