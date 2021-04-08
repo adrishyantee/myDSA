fc2ffde80666d20fc8787d0a31a9e9955b2dc5f5
adrishyantee <adrishyantee@gmail.com>
2021-04-08 12:23:00 +0530

    set and deque

---
+++ Data Structures/STL/unorderedset.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <unordered_set>
+using namespace std;
+
+int main(){
+    unordered_set<int> s;
+    s.insert(1);
+    s.insert(2);
+    s.insert(3);
+    s.insert(2);
+    for(auto i:s){
+        cout<<i<<" ";
+    }
+    cout<<endl;
+    cout<<s.size()<<endl;
+    s.erase(2);
+    for(auto i:s){
+        cout<<i<<" ";
+    }
+    return 0;
+}
---
+++ Data Structures/Stacks/largestRectange.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int get_max_area(vector<int> a){
+    int n=a.size(),ans=0,i=0;
+    a.push_back(0);
+    while(i<n){
+        while (!st.empty() and a[st.top()])
+        {
+            /* code */
+        }
+        
+    }
+
+}
+int main(){
+    vector<int> a = {2,1,5,6,2,3};
+    cout<<get_max_area(a);
+    return 0;
+}
