f1af8b82bd770104d86881f453703aad8d924733
adrishyantee <adrishyantee@gmail.com>
2021-08-06 23:45:45 +0530

    graphs

--- Data Structures/Graphs/BFS.cpp
+++ Data Structures/Graphs/BFS.cpp
@@ -1,53 +1,52 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
-const int N  = 1e+5 +2;
-bool vis[N];
-
-vector<int> adj [N];
+class Solution {
+public:
+	vector<int>bfsOfGraph(int V, vector<int> adj[]){
+	    vector<int> bfs; 
+	    vector<int> vis(V, 0); 
+	    queue<int> q; 
+	    q.push(0); 
+	    vis[0] = 1; 
+	    while(!q.empty()) {
+	        int node = q.front();
+	        q.pop(); 
+	        bfs.push_back(node); 
+	        
+	        for(auto it : adj[node]) {
+	            if(!vis[it]) {
+	                q.push(it); 
+	                vis[it] = 1; 
+	            }
+	        }
+	    }
+	    
+	    return bfs; 
+	}
+};
 
 int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int V, E;
+    	cin >> V >> E;
 
-    for (int i=0;i<N;i++){
-        vis[i]=false;
-    }
+    	vector<int> adj[V];
 
-    int n,m;
-    cin>>n>>m;
-
-    int x,y;
-    for(int i=0;i<m;i++){
-        cin>>x>>y;
-        
-    adj[x].push_back(y);
-    adj[y].push_back(x);
-    }
-
-    for(int i = 1;i<N;i++){
-        if(!vis[i]){
-
-    queue<int> q;
-
-    q.push(i);
-    vis[i]=true;
-    while(!q.empty()){
-        int node = q.front();
-        q.pop();
-        cout<<node<<endl;
-
-        vector<int>:: iterator it;
-
-        for(it=adj[node].begin();it!=adj[node].end();it++){
-            if(!vis[*it]){
-                vis[*it]=true;
-                q.push(*it);
-            }
-          }
+    	for(int i = 0; i < E; i++)
+    	{
+    		int u, v;
+    		cin >> u >> v;
+    		adj[u].push_back(v);
+    		adj[v].push_back(u);  
         }
-      }
-    }
-
- 
- return 0;
-}
+        Solution obj;
+        vector<int>ans=obj.bfsOfGraph(V, adj);
+        for(int i=0;i<ans.size();i++){
+        	cout<<ans[i]<<" ";
+        }
+        cout<<endl;
+	}
+	return 0;
